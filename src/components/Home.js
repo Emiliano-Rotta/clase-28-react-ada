e06c87ecadd3588999ff10b350dc14839384b8ea
Emiliano Rotta <emilianorottaf@gmail.com>
2023-03-08 20:59:55 -0300

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+      <div className="home_container">
+        Estoy en Home
+        <h1>
+            esto es un h1
+            <p>esto es un p</p>
+        </h1>
+      </div>
+    );
+  }
+  
+  export default Home;
